7f3eb662ad7354351fadf570d239d2c5c7ccdac8
sonjdaniel <sonjdaniel@gmail.com>
2023-06-11 18:59:23 -0500

    add comment and change postroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const postRoutes = require('./projectRoutes');
+const commentRoutes = require("./comment-routes");
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/projects', postRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
