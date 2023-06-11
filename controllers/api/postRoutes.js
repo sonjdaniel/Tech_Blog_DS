75e13d7959bec8d9d5a2babce14aee6f2928cdb5
sonjdaniel <sonjdaniel@gmail.com>
2023-06-11 18:57:47 -0500

    update post, delete post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,3 +15,42 @@ router.post("/", withAuth, async (req, res) => {
     res.status(400).json(err);
   }
 });
+// Route to update post
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const updatePost = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!updatePost) {
+      res.status(404).json({ message: "No post found with that ID" });
+      return;
+    }
+    res.status(200).json("Post has been updated");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Route to delete a post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const deletePost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletePost) {
+      res.status(404).json({ message: "No post found with that ID" });
+      return;
+    }
+    res.status(200).json("Post has been deleted");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
