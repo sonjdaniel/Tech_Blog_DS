077f87c52de8c9ff3c02596cd128e60664a56fd0
sonjdaniel <sonjdaniel@gmail.com>
2023-06-11 18:40:13 -0500

    added extra strings

--- models/Post.js
+++ models/Post.js
@@ -2,47 +2,46 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-class Project extends Model {}
+class Post extends Model {}
 
-Project.init(
+Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
-    description: {
+    content: {
       type: DataTypes.STRING,
+      allowNull: false
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    user_id: {
+    username_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'user',
-        key: 'id',
-      },
+        key: 'id'
+      }
     },
+    date_created: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+      },
   },
   {
     sequelize,
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'post',
+    createdAt: 'date_created',
+    updatedAt: false
   }
 );
 
-module.exports = Project;
+module.exports = Post;
