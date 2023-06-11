a40cdecaa573d63d5c0683101fcca9d9b6d43f98
sonjdaniel <sonjdaniel@gmail.com>
2023-06-11 18:46:36 -0500

    homeroutes add homepage, determine logout and login, auth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,81 +2,72 @@ const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+// Route to show all posts on homepage
+router.get("/", async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const postsData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["username"],
         },
       ],
     });
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+    const posts = postsData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      // This will determine if Login or Logout will show on page
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+// Route to show user's dashboard which includes their posts / This is only available if user is logged in (verified by withAuth)
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    const userPosts = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
     });
 
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
+    const posts = userPosts.get({ plain: true });
+    res.render("dashboard", {
+      ...posts,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+// Route to render newPost page / This is only available if user is logged in (verified by withAuth)
+router.get("/new-post", withAuth, (req, res) =>
+  res.render("newPost", { loggedIn: req.session.loggedIn })
+);
+
+// Route to render page that allows user to update their own posts (one at a time) / This is only available if user is logged in (verified by withAuth)
+router.get("/update/post/:id", withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
+    const onePostData = await Post.findByPk(req.params.id);
+    if (!onePostData) {
+      res.status(404).json({ message: "No posts found with that ID" });
+      return;
+    }
+    const userPost = onePostData.get({ plain: true });
+    if (userPost.username_id == req.session.user_id) {
+      res.render("updatePost", {
+        ...userPost,
+        loggedIn: req.session.loggedIn,
+      });
+    } else {
+      res
+        .status(401)
+        .json({ message: "You are not authorized to edit this post." });
+    }
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-});
-
-module.exports = router;
