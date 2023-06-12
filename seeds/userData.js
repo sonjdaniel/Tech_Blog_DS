cd011b787d63833be98f0855d0b9337bbc458bc9
sonjdaniel <sonjdaniel@gmail.com>
2023-06-11 20:58:53 -0500

    clean up

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,13 +1,17 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 const userData = [
-    {
-        username: 'wentworth',
-        email: 'wentworth@email.com',
-        password: 'aaaaaa',
-    },
-    
-]
+  {
+    username: "wentworth",
+    email: "wentworth@email.com",
+    password: "aaaaaa",
+  },
+  {
+    username: "test",
+    email: "test@email.com",
+    password: "123456789",
+  },
+];
 
 const seedUsers = () => User.bulkCreate(userData);
 
