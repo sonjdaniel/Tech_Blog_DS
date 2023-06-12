7ba2715a2bb9fb3fbcea56e8196baafbbd38f703
sonjdaniel <sonjdaniel@gmail.com>
2023-06-11 19:00:42 -0500

    finished the comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+// Route to create a new comment
+router.post("/", withAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.create({
+        // Deconstructuring req.body to add user_id
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  module.exports = router;
